25da628b1afc8e19e1abc24d1bfdd012a8fdc5b3
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-06 21:11:48 -0300

    Orientação a objetos

---
+++ Aula02/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        //Criar um objeto para representar um cachorro
+        Cachorro dogBelga = new Cachorro();
+        //Atrubuir valores aos atributos do objeto
+        dogBelga.nome = "Spike";
+        dogBelga.peso = 30;
+        dogBelga.idade = 6;
+        dogBelga.castrado = true;
+        //Exibir os dados do objeto cachorro
+        System.out.println("Cachorro 1\n");
+        System.out.println(dogBelga.nome + " tem " + dogBelga.idade + " anos e pesa " + dogBelga.peso + "kg.");
+        System.out.println("Ele é castrado? " + dogBelga.castrado);
+
+        //Criar um outro objeto cachorro
+        Cachorro dogAlemao = new Cachorro();
+        //Atribuir outros valores
+        dogAlemao.nome = "Antonio Fagundes";
+        dogAlemao.peso = 47;
+        dogAlemao.idade = 8;
+        dogAlemao.castrado = false;
+        //Exibir os valores do outro objeto
+        System.out.println("\nCachorro 2\n");
+        System.out.println(dogAlemao.nome + " tem " + dogAlemao.idade + " anos e pesa " + dogAlemao.peso + "kg.");
+        System.out.println("Ele é castrado? " + dogAlemao.castrado);
+        System.out.println("\n");
+        //Chamar os métodos
+        dogBelga.andar();
+        dogAlemao.andar();
+    }
+}
